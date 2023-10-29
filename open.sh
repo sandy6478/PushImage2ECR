9c26ba537618f4b712370905c5f1a769b2ac8ddd
Sandeep <ss8220@gmail.com>
2023-10-29 22:48:13 +0530

    first commit

---
+++ open.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/sandy6478/gitactions-ece.git
+git push -u origin main
